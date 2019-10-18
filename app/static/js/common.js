a6209a59c91296b35cff14592d2a77f19eabebaa
transoman <vasiag10@gmail.com>
2019-10-18 09:16:55 +0300

    Ready 3 sections

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -123,6 +123,10 @@ jQuery(document).ready(function($) {
 
     contentItems.hide().eq(list.find('li.active').index()).show();
 
+    list.on('click', 'a', function(e) {
+      e.preventDefault();
+    });
+
     list.on('click', 'li:not(.active)', function(e) {
       e.preventDefault();
       $(this)
@@ -147,8 +151,112 @@ jQuery(document).ready(function($) {
     });
   };
 
+  // Steps
+  let currentTab = 0; // Current tab is set to be the first tab (0)
+  showTab(currentTab); // Display the current tab
+
+  function progress() {
+    let count = $('.steps-calculate__item').length;
+
+    $('.progress__line').css({
+      width: (100/count) * (currentTab + 1) + '%'
+    });
+  };
+
+  $('.steps-calculate__btn--prev').click( function(e) {
+    e.preventDefault();
+    if (currentTab == 0) {
+      return false;
+    }
+    nextPrev(-1);
+  } );
+
+  $('.steps-calculate__btn--next').click( function(e) {
+    e.preventDefault();
+
+    if ( (currentTab + 1) >  $('.steps-calculate__item').length - 1  ) {
+      // $('.constructor-form').submit();
+      return false;
+    }
+
+    nextPrev(1);
+  } );
+
+  function showTab(n) {
+    // This function will display the specified tab of the form ...
+    let x = $('.steps-calculate__item');
+
+    let countStep = x.length;
+    let currentStep = n + 1;
+
+    $('.progress__step-current').text(currentStep);
+    $('.progress__step-total').text(countStep);
+    progress();
+
+    $(x[n]).css('display', 'block');
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+      $('.steps-calculate__btn--prev').css('display', 'none');
+    } else {
+      $('.steps-calculate__btn--prev').css('display', 'inline');
+    }
+    if (n == (x.length - 1)) {
+      $('.steps-calculate__btn--next').css('display', 'none');
+    } else {
+      $('.steps-calculate__btn--next').css('display', 'inline');
+    }
+  }
+
+  function nextPrev(n) {
+
+    // This function will figure out which tab to display
+    let x = $('.steps-calculate__item');
+
+    // Hide the current tab:
+    $(x[currentTab]).css('display', 'none');
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form... :
+    if (currentTab >= x.length) {
+      //...the form gets submitted:
+      return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+  }
+
+  // Input file
+  let inputFile = function() {
+    let inputs = $('input[type="file"]');
+
+    if (inputs.length) {
+      inputs.each(function(index, input) {
+        let label = $(this).parent();
+        let labelVal = label.find('span').text();
+
+        $(input).change(function(e) {
+          let fileName = '';
+
+          if (e.target.files.length) {
+            fileName = e.target.files[0].name;
+          }
+
+          if (fileName) {
+            label.find('span').text(fileName);
+          }
+          else {
+            label.find('span').text(labelVal);
+          }
+        });
+      });
+    }
+  };
+
+
   tabs('.portfolio-tabs', '.portfolio-tabs-list', '.portfolio-tabs__item');
+  tabs('.services-tabs', '.services-tabs-list', '.services-tabs__item');
   chosenHover();
+  inputFile();
 
   // SVG
   svg4everybody({});
--- smart-grid-config.js
+++ smart-grid-config.js
@@ -7,22 +7,21 @@ var settings = {
   offset: '30px', /* gutter width px || % || rem */
   mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
   container: {
-    maxWidth: '1740px', /* max-width оn very large screen */
-    fields: '30px' /* side fields */
+    maxWidth: '1710px', /* max-width оn very large screen */
+    fields: '15px' /* side fields */
   },
   breakPoints: {
     xl: {
-      width: '1780px', /* -> @media (max-width: 1680px) */
+      width: '1730px',
     },
     lg: {
-      width: '1260px', /* -> @media (max-width: 1100px) */
+      width: '1230px',
     },
     md: {
-      width: '1010px'
+      width: '992px'
     },
     sm: {
       width: '767px',
-      fields: '15px' /* set fields only if you want to change container.fields */
     },
     xs: {
       width: '576px'
